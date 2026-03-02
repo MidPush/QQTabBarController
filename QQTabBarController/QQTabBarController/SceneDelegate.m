9496c2cc061b0b9949fe4ee303997b87c0c65508
xz <497569855@qq.com>
2026-03-02 13:40:10 +0800

    Commit

--- QQTabBarController/QQTabBarController/SceneDelegate.m
+++ QQTabBarController/QQTabBarController/SceneDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "SceneDelegate.h"
+#import "NNTabBarController.h"
 
 @interface SceneDelegate ()
 
@@ -15,9 +16,10 @@
 
 
 - (void)scene:(UIScene *)scene willConnectToSession:(UISceneSession *)session options:(UISceneConnectionOptions *)connectionOptions {
-    // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-    // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-    // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+    self.window = [[UIWindow alloc] initWithWindowScene:(UIWindowScene *)scene];
+    NNTabBarController *mainVC = [[NNTabBarController alloc] init];
+    self.window.rootViewController = mainVC;
+    [self.window makeKeyAndVisible];
 }
 
 
